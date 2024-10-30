59b817301ed5fd6a03e6e71fc28584c3ef9acf16
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-30 19:04:59 +0530

    add inventory-service

---
+++ inventory-service/src/main/java/com/shreehar/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.shreehar.microservices.inventory.repository;
+
+import com.shreehar.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES  (100, 'iphone_16_pro'),
+        (100, 'pixel_9_pro'),
+        (100, 'galaxy_s24'),
+        (100, 'oneplus_12');
