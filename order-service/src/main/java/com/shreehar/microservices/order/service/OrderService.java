15895200133055815f970c27ebd93d6d4091f3f2
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-11-14 21:49:58 +0530

    add IPC between OS and IS

--- order-service/src/main/java/com/shreehar/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/shreehar/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.shreehar.microservices.order.service;
 
+import com.shreehar.microservices.order.client.InventoryClient;
 import com.shreehar.microservices.order.dto.OrderRequest;
 import com.shreehar.microservices.order.model.Order;
 import com.shreehar.microservices.order.repository.OrderRepository;
@@ -13,13 +14,20 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else  {
+            throw new RuntimeException("Product with Skucode "+ orderRequest.skuCode() + " is not in stock for this order");
+        }
     }
 }
